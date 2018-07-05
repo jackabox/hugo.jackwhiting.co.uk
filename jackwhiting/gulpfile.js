d26de88c10f40cb2de3341679fe76baaaee58ef3
Jack Whiting <hi@jackwhiting.co.uk>
2018-07-05 17:52:40 +0100

    unsubmodule

---
+++ jackwhiting/gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+
+gulp.task('sass', () => {
+    gulp.src('static/scss/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['> 1%', 'last 5 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('static/css'))
+})
+
+gulp.task('default', ['sass'], () => {
+    gulp.watch('static/scss/*.scss', ['sass'])
+})
